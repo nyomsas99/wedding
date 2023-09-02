8d6c2094921cac1c807b564909535250960bece1
nyomsas99 <nyomsas1234@gmail.com>
2023-09-02 13:56:59 +0700

    last episode

--- js/script.js
+++ js/script.js
@@ -28,6 +28,11 @@ offcanvas.addEventListener("hidden.bs.offcanvas", function () {
 
 // scrol; button
 const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector(".audio-icon-wrapper");
+const song = document.querySelector("#song");
+let isPlaying = false;
+const audioIcon = document.querySelector(".audio-icon-wrapper i");
+
 function disableScroll() {
   scrollTop = window.pageYOffset || document.elementFromPoint.scrollTop;
   scrollLeft = window.pageXOffset || document.elementFromPoint.scrollLeft;
@@ -42,12 +47,36 @@ function disableScroll() {
 function enableScroll() {
   window.onscroll = function () {
     rootElement.style.scrollBehavior = "smooth";
-    localStorage.setItem("opened", "true");
+    // localStorage.setItem("opened", "true");
+    playAudio();
   };
+
+  function playAudio() {
+    song.voume = 0.1;
+    audioIconWrapper.style.display = "flex";
+    song.play();
+    isPlaying = true;
+  }
 }
-if (!localStorage.getItem("opened")) {
-  disableScroll();
-}
+
+audioIconWrapper.onclick = function () {
+  if (isPlaying) {
+    song.pause();
+    audioIcon.classList.remove("bi-disc");
+    audioIcon.classList.add("bi-pause-circle");
+  } else {
+    song.play();
+    audioIcon.classList.add("bi-disc");
+    audioIcon.classList.remove("bi-pause-circle");
+  }
+
+  isPlaying = !isPlaying;
+};
+
+disableScroll();
+// if (!localStorage.getItem("opened")) {
+//   disableScroll();
+// }
 
 // form
 window.addEventListener("load", function () {
@@ -64,3 +93,12 @@ window.addEventListener("load", function () {
     });
   });
 });
+
+// nama user yang di undang
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get("n") || "";
+const namaContainer = document.querySelector(".hero h4 span");
+const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
+
+document.querySelector("#nama").value = nama;
